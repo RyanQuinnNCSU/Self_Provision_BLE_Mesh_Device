55bf1f044938557a060859ddf7f4cdf534d1af1b
Tech Query <rtquinn2@ncsu.edu>
2020-07-15 21:51:37 -0400

    Set up SP files`

---
+++ soc-btmesh-sensor-client/Self-Provision_Functions.h
@@ -0,0 +1,13 @@
+/*
+ * Self-Provision_Functions.h
+ *
+ *  Created on: Jul 15, 2020
+ *      Author: rquin
+ */
+
+#ifndef SELF_PROVISION_FUNCTIONS_H_
+#define SELF_PROVISION_FUNCTIONS_H_
+
+
+
+#endif /* SELF_PROVISION_FUNCTIONS_H_ */
